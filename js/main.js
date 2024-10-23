dd4151795bbafc3459b501f3990143bf60ad4980
GiorgiaSecchi <secchigrg@gmail.com>
2024-10-23 16:39:27 +0200

    add preventDefault

--- js/main.js
+++ js/main.js
@@ -3,9 +3,18 @@ const kilometersIput = document.getElementById("input-Kilometers");
 const ageRangeIput = document.getElementById("input-AgeRange");
 const submitButtonIput = document.getElementById("submit-button");
 const cancelButtonIput = document.getElementById("cancel-button");
+const personalTicketInfo = document.getElementById("personal-ticket-info");
 
-console.log(nameIput);
-console.log(kilometersIput);
-console.log(ageRangeIput);
-console.log(submitButtonIput);
-console.log(cancelButtonIput);
+// console.log(nameIput);
+// console.log(kilometersIput);
+// console.log(ageRangeIput);
+// console.log(submitButtonIput);
+// console.log(cancelButtonIput);
+
+personalTicketInfo.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const name = nameIput.value;
+  const kilometers = nameIput.value;
+  const ageRange = nameIput.value;
+});
