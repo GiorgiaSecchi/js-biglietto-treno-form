5d5e318a6b4b0b2ed1616b5c299e74546e02c350
GiorgiaSecchi <secchigrg@gmail.com>
2024-10-23 16:13:23 +0200

    get element Id

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("JS OK");
+const nameIput = document.getElementById("input-Name");
+const kilometersIput = document.getElementById("input-Kilometers");
+const ageRangeIput = document.getElementById("input-AgeRange");
+const submitButtonIput = document.getElementById("submit-button");
+const cancelButtonIput = document.getElementById("cancel-button");
+
+console.log(nameIput);
+console.log(kilometersIput);
+console.log(ageRangeIput);
+console.log(submitButtonIput);
+console.log(cancelButtonIput);
