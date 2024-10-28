d885904cb159305a0d16839df5e7e8a66df00ef9
GiorgiaSecchi <secchigrg@gmail.com>
2024-10-28 17:10:09 +0100

    mod eventlistener form

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const nameInput = document.getElementById("input-Name");
 const kilometersInput = document.getElementById("input-Kilometers");
-const ageRangeInput = document.getElementById("input-AgeRange");
+const ageRangeInIput = document.getElementById("age-range");
 const submitButtonInput = document.getElementById("submit-button");
 const cancelButtonnIput = document.getElementById("cancel-button");
 const personalTicketInfo = document.getElementById("personal-ticket-info");
@@ -10,23 +10,28 @@ const priceOutput = document.getElementById("output-price");
 
 // console.log(nameIput);
 // console.log(kilometersIput);
-// console.log(ageRangeIput);
+// console.log(ageRangeInIputIput);
 // console.log(submitButtonIput);
 // console.log(cancelButtonIput);
 
-const ageRange = ageRangeInput.value;
+// const ageRangeInIput = ageRangeInIput.value;
 
-const kilometers = Number(kilometersInput.value);
-console.log(kilometers);
+// const kilometers = Number(kilometersInput.value);
+// console.log(kilometers);
 
-let milesPrice = 0.21;
-const ticketPrice = kilometers * milesPrice;
-// priceOutput.textContent = ticketPrice;
-console.log(ticketPrice);
+// let milesPrice = 0.21;
+// const ticketPrice = kilometers * milesPrice;
+// // priceOutput.textContent = ticketPrice;
+// console.log(ticketPrice);
 
-submitButtonInput.addEventListener("click", () => {
-  const name = nameInput.value.trim();
-  nameOutput.textContent = name;
+personalTicketInfo.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const name = nameInput.value;
+  const kilometers = kilometersInput.value;
+  const ageRange = ageRangeInIput.value;
+
+  console.log(name, kilometers, ageRange);
 });
 
 // personalTicketInfo.addEventListener("submit", (event) => {
@@ -34,6 +39,6 @@ submitButtonInput.addEventListener("click", () => {
 
 //   const name = nameInput.value.trim();
 //   const kilometers = kilometersInput.value.trim();
-//   const ageRange = ageRangeInput.value.trim();
+//   const ageRangeInIput = ageRangeInIput.value.trim();
 
 // });
